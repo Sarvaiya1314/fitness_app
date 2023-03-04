c748d63f954eabbc342bf0bebf60c8cd73deea77
Chirag Sarvaiya <chiragsarvaiya19124@gmail.com>
2023-03-04 12:43:01 +0530

    c
    .e

---
+++ lib/chirag/finess_trainers_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FitnessTrainersScreen extends StatefulWidget {
+  const FitnessTrainersScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FitnessTrainersScreen> createState() => _FitnessTrainersScreenState();
+}
+
+class _FitnessTrainersScreenState extends State<FitnessTrainersScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1C1C1E),
+    );
+  }
+}
